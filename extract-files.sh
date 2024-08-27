3b14e9da14c9f20f953bc0db4c0a24bec095018b
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:51:30 +0300

    wayne-common: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/lib/libMiWatermark.so)
+            [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib/hw/camera.sdm660.so)
             perl -pi -e 's/\xc1\x68\xd0\xe9\x0d\x20\xcd\xe9\x03\x20/\xc1\x68\xd0\xe9\x12\x20\xcd\xe9\x03\x20/g' "${2}"
             perl -pi -e 's/\xdb\xf8\x00\x10\x08\x9a\x49\x6b\xc6\xe9\x04\x21/\xdb\xf8\x00\x10\x08\x9a\x89\x6c\xc6\xe9\x04\x21/g' "${2}"
+        *)
+            return 1
             ;;
     esac
+
+    return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -29,4 +37,4 @@ set -e
 export DEVICE_COMMON=sdm660-common
 export DEVICE_SPECIFIED_COMMON_DEVICE="jasmine_sprout wayne"
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
